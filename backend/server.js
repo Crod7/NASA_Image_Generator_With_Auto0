f9fba49216a3a23ae679caaebe685794854afa9e
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-06-24 19:31:41 -0700

    Added core libraries to set up server, cookie-session, cors, passport.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cookieSession(
+  {
+    name:"session",
+    keys:["abc"],
+    maxAge:24 * 60 * 60 * 100,
+  }
+))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+  origin: "*",
+  method: "GET,POST,PUT,DELETE",
+  credentials: true
+}))
 
 app.get('/', (req, res) => {
   res.send("You're finally awake.");

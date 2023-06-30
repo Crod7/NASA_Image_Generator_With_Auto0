8503f85714fd105fcf4f2e379afeaaacedc3ad62
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-06-30 10:45:03 -0700

    Attempting to deploy changing cors

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -8,7 +8,6 @@ const corsOptions = {
         "http://localhost:5000",
         "https://lh3.googleusercontent.com/", // This allows user's profile image from google to be obtained
         "https://nasa-daily-image-generator-api.onrender.com/",
-        
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true // Enable sending cookies with the request
--- frontend/src/config/backendURL.js
+++ frontend/src/config/backendURL.js
@@ -1,5 +1,6 @@
 // This file was made to easily change the url throughout all fetch requests.
 
 const backendURL = 'https://nasa-daily-image-generator-api.onrender.com';
+//const backendURL = 'http://localhost:5000'
 
 module.exports = backendURL;
